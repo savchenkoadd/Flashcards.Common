d635c280c1089a4c518248dec480ea35c0ba31c1
savchenkoadd <savchenkooad@gmail.com>
2024-01-31 16:56:03 +0200

    Add flashcard DTOs

---
+++ src/Flashcards.Common/Flashcards.Common/DTO/Flashcard/FlashcardApiRequest.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Flashcards.Common.DTO.Flashcard
+{
+    public class FlashcardApiRequest
+    {
+        [Required]
+        public Guid CardId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string? MainSide { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string? OppositeSide { get; set; }
+
+        [Required]
+        [Range(0, float.MaxValue)]
+        public float EFactor { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int RepetitionCount { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateOnly NextRepeatDate { get; set; }
+
+        public bool WhetherToDelete { get; set; }
+    }
+}
