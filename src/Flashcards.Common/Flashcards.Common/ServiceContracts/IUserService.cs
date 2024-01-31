299b6d9194c8c2eeeb6f11e8c88119caa35a8d15
savchenkoadd <savchenkooad@gmail.com>
2024-01-31 17:18:56 +0200

    Implement UserService

---
+++ src/Flashcards.Common/Flashcards.Common/ServiceContracts/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Flashcards.Common.DTO.Identity;
+
+namespace Flashcards.Common.ServiceContracts
+{
+	public interface IUserService
+	{
+		Task Login(LoginDTO? loginDTO);
+
+		Task Register(RegisterDTO? registerDTO);
+
+		Task Logout();
+	}
+}
---
+++ src/Flashcards.Common/Flashcards.Common/Services/UserService.cs
@@ -0,0 +1,46 @@
+﻿using Flashcards.Common.DTO.Identity;
+using Flashcards.Common.RepositoryContracts;
+using Flashcards.Common.ServiceContracts;
+using FlashCards.Common.Helpers;
+
+namespace Flashcards.Common.Services
+{
+	public class UserService : IUserService
+	{
+		private readonly IApiRepository _apiRepository;
+		private bool _isLoggedIn;
+
+        public UserService(IApiRepository apiRepository)
+        {
+            _apiRepository = apiRepository;
+			_isLoggedIn = false;
+        }
+
+        public async Task Login(LoginDTO? loginDTO)
+		{
+			await ValidationHelper.ValidateObjects(loginDTO);
+
+			await _apiRepository.Login(loginDTO!);
+			_isLoggedIn = true;
+		}
+
+		public async Task Logout()
+		{
+			if (!_isLoggedIn)
+			{
+				throw new InvalidOperationException("Unable to logout. You must be logged in to logout.");
+			}
+
+			await _apiRepository.Logout();
+			_isLoggedIn = false;
+		}
+
+		public async Task Register(RegisterDTO? registerDTO)
+		{
+			await ValidationHelper.ValidateObjects(registerDTO);
+
+			await _apiRepository.Register(registerDTO!);
+			_isLoggedIn = true;
+		}
+	}
+}
