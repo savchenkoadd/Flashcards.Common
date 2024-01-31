baf7079f8b2a8facd568560b7c203f749570b01d
savchenkoadd <savchenkooad@gmail.com>
2024-01-31 16:58:15 +0200

    Add identity DTOs

---
+++ src/Flashcards.Common/Flashcards.Common/DTO/Identity/LoginDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Flashcards.Common.DTO.Identity
+{
+	public class LoginDTO
+	{
+		[Required]
+		[EmailAddress]
+		[StringLength(50)]
+		public string? Email { get; set; }
+
+		[Required]
+		public string? Password { get; set; }
+	}
+}
---
+++ src/Flashcards.Common/Flashcards.Common/DTO/Identity/RegisterDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Flashcards.Common.DTO.Identity
+{
+	public class RegisterDTO
+	{
+		[Required]
+		[StringLength(20)]
+		public string? PersonName { get; set; }
+
+		[Required]
+		[EmailAddress]
+		[StringLength(50)]
+		public string? Email { get; set; }
+
+		[Required]
+		[StringLength(15)]
+		[Phone]
+		public string? PhoneNumber { get; set; }
+
+		[Required]
+		public string? Password { get; set; }
+
+		[Required]
+		[Compare(nameof(Password))]
+		public string? ConfirmPassword { get; set; }
+	}
+}
