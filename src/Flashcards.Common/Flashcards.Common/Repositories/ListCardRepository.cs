9711716bd6dade14370bd2d55455a188ba31f3bf
savchenkoadd <savchenkooad@gmail.com>
2024-01-31 18:57:38 +0200

    Implement CardRepository based on a list

---
+++ src/Flashcards.Common/Flashcards.Common/Repositories/ListCardRepository.cs
@@ -0,0 +1,55 @@
+﻿using Flashcards.Common.Entities;
+using Flashcards.Common.RepositoryContracts;
+using System.Linq.Expressions;
+
+namespace Flashcards.Common.Repositories
+{
+	public class ListCardRepository : ICardRepository
+	{
+		private List<Flashcard> _repository;
+
+        public ListCardRepository()
+        {
+            _repository = new List<Flashcard>();
+        }
+
+        public async Task CreateAsync(Flashcard flashcard)
+		{
+			_repository.Add(flashcard);
+
+			await Task.CompletedTask;
+		}
+
+		public async Task DeleteAsync(Guid cardId)
+		{
+			_repository.RemoveAll(temp => temp.CardId == cardId);
+
+			await Task.CompletedTask;
+		}
+
+		public async Task<IEnumerable<Flashcard>?> GetAllAsync(Expression<Func<Flashcard, bool>> expression)
+		{
+			return await Task.FromResult(_repository.Where(expression.Compile()));
+		}
+
+		public async Task<Flashcard?> GetByIdAsync(Guid cardId)
+		{
+			return await Task.FromResult(_repository.FirstOrDefault(temp => temp.CardId == cardId));
+		}
+
+		public async Task ReplaceAllAsync(IEnumerable<Flashcard> replacement)
+		{
+			_repository = replacement.ToList();
+
+			await Task.CompletedTask;
+		}
+
+		public async Task UpdateAsync(Guid cardId, Flashcard flashcard)
+		{
+			await DeleteAsync(cardId);
+			_repository.Add(flashcard);
+
+			await Task.CompletedTask;
+		}
+	}
+}
---
+++ src/Flashcards.Common/Flashcards.Common/RepositoryContracts/ICardRepository.cs
@@ -0,0 +1,20 @@
+﻿using Flashcards.Common.Entities;
+using System.Linq.Expressions;
+
+namespace Flashcards.Common.RepositoryContracts
+{
+	public interface ICardRepository
+	{
+		Task CreateAsync(Flashcard flashcard);
+
+		Task UpdateAsync(Guid cardId, Flashcard flashcard);
+
+		Task DeleteAsync(Guid cardId);
+
+		Task ReplaceAllAsync(IEnumerable<Flashcard> replacement);
+
+		Task<Flashcard?> GetByIdAsync(Guid cardId);
+
+		Task<IEnumerable<Flashcard>?> GetAllAsync(Expression<Func<Flashcard, bool>> expression);
+	}
+}
