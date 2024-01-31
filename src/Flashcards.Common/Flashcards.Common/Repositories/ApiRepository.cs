895ec9a840381075f93639c3440ff4c0323b1b4e
savchenkoadd <savchenkooad@gmail.com>
2024-01-31 17:11:58 +0200

    Implement ApiRepository.cs

---
+++ src/Flashcards.Common/Flashcards.Common/Repositories/ApiRepository.cs
@@ -0,0 +1,65 @@
+﻿using Flashcards.Common.DTO.Flashcard;
+using Flashcards.Common.DTO.Identity;
+using Flashcards.Common.RepositoryContracts;
+using System.Net.Http.Json;
+
+namespace Flashcards.Common.Repositories
+{
+	public class ApiRepository : IApiRepository
+	{
+		private readonly HttpClient _httpClient;
+		private readonly string _connectionString;
+		private readonly string _loginEndpointPath;
+		private readonly string _logoutEndpointPath;
+		private readonly string _registerEndpointPath;
+		private readonly string _syncEndpointPath;
+
+		public ApiRepository(
+				string connectionString,
+				string loginEndpointPath = nameof(Login),
+				string logoutEndpointPath = nameof(Logout),
+				string registerEndpointPath = nameof(Register),
+				string syncEndpointPath = nameof(SyncAndGetCards)
+			)
+        {
+            _connectionString = connectionString;
+			_loginEndpointPath = loginEndpointPath;
+			_logoutEndpointPath = logoutEndpointPath;
+			_registerEndpointPath = registerEndpointPath;
+			_syncEndpointPath = syncEndpointPath;
+			_httpClient = new HttpClient();
+        }
+
+        public async Task Login(LoginDTO loginDTO)
+		{
+			var result = await _httpClient.PostAsJsonAsync(_connectionString + _loginEndpointPath, loginDTO);
+
+			result.EnsureSuccessStatusCode();
+		}
+
+		public async Task Logout()
+		{
+			var result = await _httpClient.GetAsync(_connectionString + _logoutEndpointPath);
+
+			result.EnsureSuccessStatusCode();
+		}
+
+		public async Task Register(RegisterDTO registerDTO)
+		{
+			var result = await _httpClient.PostAsJsonAsync(_connectionString + _registerEndpointPath, registerDTO);
+
+			result.EnsureSuccessStatusCode();
+		}
+
+		public async Task<IEnumerable<FlashcardApiResponse>?> SyncAndGetCards(IEnumerable<FlashcardApiRequest> flashcards)
+		{
+			var responseMessage = await _httpClient.PostAsJsonAsync(_connectionString + _syncEndpointPath, flashcards);
+
+			responseMessage.EnsureSuccessStatusCode();
+
+			var apiFlashcards = await responseMessage.Content.ReadFromJsonAsync<IEnumerable<FlashcardApiResponse>>();
+
+			return apiFlashcards;
+		}
+	}
+}
---
+++ src/Flashcards.Common/Flashcards.Common/RepositoryContracts/IApiRepository.cs
@@ -0,0 +1,16 @@
+﻿using Flashcards.Common.DTO.Flashcard;
+using Flashcards.Common.DTO.Identity;
+
+namespace Flashcards.Common.RepositoryContracts
+{
+	public interface IApiRepository
+	{
+		Task Register(RegisterDTO registerDTO);
+
+		Task Login(LoginDTO loginDTO);
+
+		Task Logout();
+
+		Task<IEnumerable<FlashcardApiResponse>?> SyncAndGetCards(IEnumerable<FlashcardApiRequest> flashcards);
+	}
+}
